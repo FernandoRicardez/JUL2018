8eaf0bb03848234d0ef46d5885f94f34281141f3
57LECS <eddsc57@gmail.com>
2018-08-12 17:26:39 -0500

    added de solutions and initial proyect views

---
+++ Lasallistas/Data/Remote.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Connectivity;
+
+namespace Lasallistas.Data
+{
+    public class Remote
+    {
+        public delegate void StringCallback(string response, string error);
+
+        #if DEBUG
+        public bool UseLocalDemo { get => false; }
+        string Server { get => "http://192.168.0.13:8080/web"; }
+        //string Server { get => "https://www.idc.edu.mx/QA/Jesuitas_webServices/web"; }
+        #else
+        public bool UseLocalDemo { get => false; }
+        string Server { get => "https://www.idc.edu.mx/QA/Jesuitas_webServices/web"; }
+        #endif
+        public string ApiDailyMenu { get => Server + "/api/portalfamiliar/parents/{parentId}/daily-menu"; }
+        public string ApiHygiene { get => Server + "/api/portalfamiliar/parents/{parentId}/hygiene"; }
+        public string ApiLogin { get => Server + "/api/portalfamiliar/login"; }
+        public string ApiNews { get => Server + "/api/portalfamiliar/parents/{parentId}/news"; }
+        public string ApiNewsSearch { get => Server + "/api/portalfamiliar/parents/{parentId}/news/search"; }
+        public string ApiPasswordChange { get => Server + "/api/portalfamiliar/CambiarPassword"; }
+        public string ApiPasswordRecovery { get => Server + "/api/portalfamiliar/RecuperarPassword"; }
+        public string ApiReport { get => Server + "/api/portalfamiliar/parents/{parentId}/report"; }
+        public string ApiStock { get => Server + "/api/portalfamiliar/parents/{parentId}/stock"; }
+        public string ApiStudentsByParent { get => Server + "/api/portalfamiliar/AlumnoPorPadreTutor/{parentId}"; }
+
+        static readonly Remote _shared = new Remote();
+
+        public static Remote Shared => _shared;
+
+        public async Task RequestString(string method, string path, Dictionary<string, string> headers, string data, StringCallback onResponse)
+        {
+            if (!CrossConnectivity.Current.IsConnected)
+            {
+                onResponse?.Invoke(null, "Sin conexión");
+                return;
+            }
+                
+            #if __IOS__
+            UIKit.UIApplication.SharedApplication.NetworkActivityIndicatorVisible = true;
+            #endif
+            Console.WriteLine($"{method} {path}");
+            var request = WebRequest.CreateHttp(path);
+            request.Method = method;
+
+            if (headers != null)
+            {
+                foreach (var header in headers)
+                {
+                    Console.WriteLine($"{header.Key}: {header.Value}");
+                    if (header.Key == "Content-Type")
+                    {
+                        request.ContentType = header.Value;
+                    }
+                    else
+                    {
+                        request.Headers.Add(header.Key, header.Value);
+                    }
+                }
+            }
+
+            string error = null;
+            string stringResponse = null;
+            try
+            {
+                if (data != null)
+                {
+                    if (headers == null || headers["Content-Type"] == null)
+                    {
+                        Console.WriteLine("Content-Type: application/x-www-form-urlencoded");
+                        request.ContentType = "application/x-www-form-urlencoded";
+                    }
+                    Console.WriteLine($"DATA: {data}");
+                    var bytes = Encoding.UTF8.GetBytes(data);
+                    var requestStream = await request.GetRequestStreamAsync();
+                    requestStream.Write(bytes, 0, bytes.Length);
+                    requestStream.Close();
+                }
+
+                var response = await request.GetResponseAsync();
+                stringResponse = await new StreamReader(response.GetResponseStream()).ReadToEndAsync();
+                Console.WriteLine($"RESPONSE: {stringResponse}\n");
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e);
+                error = e.Message;
+            }
+            #if __IOS__
+            UIKit.UIApplication.SharedApplication.NetworkActivityIndicatorVisible = false;
+            #endif
+            onResponse?.Invoke(stringResponse, error);
+        }
+    }
+}
---
+++ Lasallistas/Models/Universidad.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using Lasallistas.Data;
+using Newtonsoft.Json;
+
+namespace Lasallistas.Models
+{
+    public delegate void Callback(List<DailyMenu> dailyMenus, string message);
+
+    public class Universidad
+    {
+        public long Id { get; set; }
+        public string Nombre { get; set; }
+        public string Siglas { get; set; }
+        public string Abreviatura { get; set; }
+        public int Students_In_Group { get; set; }
+
+        public static void GetRemoteDataForParent(long parentId, int year, int month, Callback callback)
+        {
+            if (parentId <= 0)
+            {
+                callback?.Invoke(null, "El identificador de usuario no es válido.");
+                return;
+            }
+
+            List<DailyMenu> list = null;
+            if (Remote.Shared.UseLocalDemo)
+            {
+                list = new List<DailyMenu>
+                {
+                    new DailyMenu
+                    {
+                        Id = 1,
+                        Date = new DateTime(2018, month, 1),
+                        Student_Id = 10,
+                        Student_Name = "Miguel Hernández Martinez",
+                        Students_In_Group = 25,
+                        Items = new List<DailyMenuItem>
+                        {
+                            new DailyMenuItem
+                            {
+                                Id = 1,
+                                Title = "Bollos con jamón",
+                                Details = "Bollos con mostaza y mayonesa, jamón y queso amarillo."
+                            },
+                            new DailyMenuItem
+                            {
+                                Id = 2,
+                                Title = "Fruta picada",
+                                Details = "Manzana, melón y mango con un poco de yogurt natural."
+                            },
+                            new DailyMenuItem
+                            {
+                                Id = 3,
+                                Title = "Bollos con jamón",
+                                Details = "Bollos con mostaza y mayonesa, jamón y queso amarillo."
+                            },
+                            new DailyMenuItem
+                            {
+                                Id = 4,
+                                Title = "Fruta picada",
+                                Details = "Manzana, melón y mango con un poco de yogurt natural."
+                            }
+                        }
+                    },
+                    new DailyMenu
+                    {
+                        Id = 1,
+                        Date = new DateTime(2018, month, 3),
+                        Student_Id = 1,
+                        Student_Name = "Luis Luna Vallejo",
+                        Students_In_Group = 25,
+                        Items = new List<DailyMenuItem>
+                        {
+                            new DailyMenuItem
+                            {
+                                Id = 1,
+                                Title = "Bollos con jamón",
+                                Details = "Bollos con mostaza y mayonesa, jamón y queso amarillo."
+                            },
+                            new DailyMenuItem
+                            {
+                                Id = 2,
+                                Title = "Fruta picada",
+                                Details = "Manzana, melón y mango con un poco de yogurt natural."
+                            },
+                            new DailyMenuItem
+                            {
+                                Id = 3,
+                                Title = "Bollos con jamón",
+                                Details = "Bollos con mostaza y mayonesa, jamón y queso amarillo."
+                            },
+                            new DailyMenuItem
+                            {
+                                Id = 4,
+                                Title = "Fruta picada",
+                                Details = "Manzana, melón y mango con un poco de yogurt natural."
+                            }
+                        }
+                    },
+                    new DailyMenu
+                    {
+                        Id = 1,
+                        Date = new DateTime(2018, month, 18),
+                        Student_Id = 2,
+                        Student_Name = "Alejandra Luna Vallejo",
+                        Students_In_Group = 25,
+                        Items = new List<DailyMenuItem>
+                        {
+                            new DailyMenuItem
+                            {
+                                Id = 1,
+                                Title = "Bollos con jamón",
+                                Details = "Bollos con mostaza y mayonesa, jamón y queso amarillo."
+                            },
+                            new DailyMenuItem
+                            {
+                                Id = 2,
+                                Title = "Fruta picada",
+                                Details = "Manzana, melón y mango con un poco de yogurt natural."
+                            },
+                            new DailyMenuItem
+                            {
+                                Id = 3,
+                                Title = "Bollos con jamón",
+                                Details = "Bollos con mostaza y mayonesa, jamón y queso amarillo."
+                            },
+                            new DailyMenuItem
+                            {
+                                Id = 4,
+                                Title = "Fruta picada",
+                                Details = "Manzana, melón y mango con un poco de yogurt natural."
+                            }
+                        }
+                    },
+                    new DailyMenu
+                    {
+                        Id = 1,
+                        Date = new DateTime(2018, month, 27),
+                        Student_Id = 4,
+                        Student_Name = "Javier Luna Hernández",
+                        Students_In_Group = 25,
+                        Items = new List<DailyMenuItem>
+                        {
+                            new DailyMenuItem
+                            {
+                                Id = 1,
+                                Title = "Bollos con jamón",
+                                Details = "Bollos con mostaza y mayonesa, jamón y queso amarillo."
+                            },
+                            new DailyMenuItem
+                            {
+                                Id = 2,
+                                Title = "Fruta picada",
+                                Details = "Manzana, melón y mango con un poco de yogurt natural."
+                            },
+                            new DailyMenuItem
+                            {
+                                Id = 3,
+                                Title = "Bollos con jamón",
+                                Details = "Bollos con mostaza y mayonesa, jamón y queso amarillo."
+                            },
+                            new DailyMenuItem
+                            {
+                                Id = 4,
+                                Title = "Fruta picada",
+                                Details = "Manzana, melón y mango con un poco de yogurt natural."
+                            }
+                        }
+                    }
+                };
+
+                callback?.Invoke(list, null);
+            }
+            else
+            {
+                var url = Remote.Shared.ApiDailyMenu.Replace("{parentId}", parentId.ToString()) + $"?year={year}&month={month}";
+                var _ = Remote.Shared.RequestString("GET", url, null, null, (response, error) =>
+                {
+                    string message = null;
+                    if (error != null)
+                    {
+                        message = error;
+                    }
+                    else
+                    {
+                        try
+                        {
+                            list = JsonConvert.DeserializeObject<List<DailyMenu>>(response);
+                        }
+                        catch (Exception)
+                        {
+                            message = response;
+                        }
+                    }
+                    callback?.Invoke(list, message);
+                });
+            }
+        }
+    }
+}
