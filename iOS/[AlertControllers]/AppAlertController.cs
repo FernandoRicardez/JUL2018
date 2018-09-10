583a9b43fb07d0dd3d26da9233aeb32c79d3f7bf
Andres Conde <andresconder@gmail.com>
2018-09-10 17:14:10 -0500

    Diseño
    Se añade parte de mapa universitario.

--- iOS/[AlertControllers]/AppAlertController.cs
+++ iOS/[AlertControllers]/AppAlertController.cs
@@ -15,36 +15,36 @@ namespace Lasallistas.iOS.AlertControllers
         {
         }
 
-        public void AddAction(string title, ActionStyle style, EventHandler handler)
-        {
-            var button = new UIButton(UIButtonType.Custom)
-            {
-                BackgroundColor = style == ActionStyle.Accent ? new UIColor(0xCF / 255f, 0x08 / 255f, 0x20 / 255f, 1) : new UIColor(0x22 / 255f, 0x33 / 255f, 0x44 / 255f, 1)
-            };
-            button.SetTitle(title, UIControlState.Normal);
-            button.TouchUpInside += (sender, e) =>
-            {
-                handler?.Invoke(sender, e);
-                DismissViewController(true, null);
-            };
-            actionStack.AddArrangedSubview(button);
-        }
+        //public void AddAction(string title, ActionStyle style, EventHandler handler)
+        //{
+        //    var button = new UIButton(UIButtonType.Custom)
+        //    {
+        //        BackgroundColor = style == ActionStyle.Accent ? new UIColor(0xCF / 255f, 0x08 / 255f, 0x20 / 255f, 1) : new UIColor(0x22 / 255f, 0x33 / 255f, 0x44 / 255f, 1)
+        //    };
+        //    button.SetTitle(title, UIControlState.Normal);
+        //    button.TouchUpInside += (sender, e) =>
+        //    {
+        //        handler?.Invoke(sender, e);
+        //        DismissViewController(true, null);
+        //    };
+        //    actionStack.AddArrangedSubview(button);
+        //}
 
-        public static AppAlertController Create(string title, string message)
-        {
-            var controller = UIStoryboard.FromName("Main", null).InstantiateViewController("AppAlertController") as AppAlertController;
-            var _ = controller.View;    // Call it's view to preload outlets.
-            controller.titleLabel.Text = title;
-            controller.messageLabel.Text = message;
-            foreach (UIView view in controller.actionStack.Subviews)
-            {
-                controller.actionStack.RemoveArrangedSubview(view);
-                view.RemoveFromSuperview();
-            }
-            controller.View.BackgroundColor = new UIColor(0, 0.4f);
-            controller.ModalPresentationStyle = UIModalPresentationStyle.OverFullScreen;
-            controller.ModalTransitionStyle = UIModalTransitionStyle.CrossDissolve;
-            return controller;
-        }
+        //public static AppAlertController Create(string title, string message)
+        //{
+        //    var controller = UIStoryboard.FromName("Main", null).InstantiateViewController("AppAlertController") as AppAlertController;
+        //    var _ = controller.View;    // Call it's view to preload outlets.
+        //    controller.titleLabel.Text = title;
+        //    controller.messageLabel.Text = message;
+        //    foreach (UIView view in controller.actionStack.Subviews)
+        //    {
+        //        controller.actionStack.RemoveArrangedSubview(view);
+        //        view.RemoveFromSuperview();
+        //    }
+        //    controller.View.BackgroundColor = new UIColor(0, 0.4f);
+        //    controller.ModalPresentationStyle = UIModalPresentationStyle.OverFullScreen;
+        //    controller.ModalTransitionStyle = UIModalTransitionStyle.CrossDissolve;
+        //    return controller;
+        //}
     }
 }
